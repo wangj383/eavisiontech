db49fed710e34c773a2e3cafce3b4b3bd66c9dd9
wangj383 <57647231+wangj383@users.noreply.github.com>
2020-07-07 18:40:25 -0400

    first commit

---
+++ static/header/header.php
@@ -0,0 +1,12 @@
+<nav class="navigation">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<img class="logo" src="../img/logo.png">
+			</div>
+			<div class="col-md-6">
+				
+			</div>
+		</div>
+	</div>
+</nav>
---
+++ static/js/accordion.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    // Add minus icon for collapse element which is open by default
+    $(".collapse.in").each(function(){
+    	$(this).siblings(".panel-heading").find(".glyphicon").addClass("glyphicon-minus").removeClass("glyphicon-plus");
+    });
+    
+    // Toggle plus minus icon on show hide of collapse element
+    $(".collapse").on('show.bs.collapse', function(){
+    	$(this).parent().find(".glyphicon").removeClass("glyphicon-plus").addClass("glyphicon-minus");
+    }).on('hide.bs.collapse', function(){
+    	$(this).parent().find(".glyphicon").removeClass("glyphicon-minus").addClass("glyphicon-plus");
+    });
+});
